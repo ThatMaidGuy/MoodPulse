28a1099e8f1a115b8c715cf463ba153ecf258f80
Dmitriy Volkov <37198050+ThatMaidGuy@users.noreply.github.com>
2025-12-28 02:12:57 +0500

    change icon

--- scripts/hint.gd
+++ scripts/hint.gd
@@ -5,7 +5,7 @@ func show_hint(text: String) -> void:
 	show()
 	get_node("margin/Label").text = text
 	var t = get_tree().create_tween()
-	t.tween_property($"../../Hint", "modulate", Color(1,1,1,1), 0.5)
+	t.tween_property(self, "modulate", Color(1,1,1,1), 0.5)
 	t.tween_interval(3.0)
-	t.tween_property($"../../Hint", "modulate", Color(1,1,1,0), 0.5)
+	t.tween_property(self, "modulate", Color(1,1,1,0), 0.5)
 	t.tween_callback(hide)
